453992e4e6e3b52a2fd52d6508b58deba5b0ffa3
Jason Gassner <byrd8378160@outlook.com>
2018-08-29 20:57:14 +0000

    spc.py edited online with Bitbucket

--- spc.py
+++ spc.py
@@ -11,54 +11,59 @@ class Outlooks(enum.Enum):
     DAY4 = 3
 
 
-class ConvectiveOutlook:
+class ConvectiveOutlook(object):
+    @property
+    def base_url(self):
+        if self.forecast_period is Outlooks.DAY4:
+            return r'https://www.spc.noaa.gov/products/exper/'
+        else:
+            return r'https://www.spc.noaa.gov/products/outlook/'
+
+    @property
+    def url(self):
+        if self.forecast_period is Outlooks.DAY1:
+            return self.base_url + 'day1otlk.html'
+        elif self.forecast_period is Outlooks.DAY2:
+            return self.base_url + 'day2otlk.html'
+        elif self.forecast_period is Outlooks.DAY3:
+            return self.base_url + 'day3otlk.html'
+        elif self.forecast_period is Outlooks.DAY4:
+            return self.base_url + 'day4-8/'
+        else:
+            raise IndexError("That outlook doesn't exist.")
+
+    @property
+    def forecast_period(self):
+        return self.__forecast_period
+
+    @forecast_period.setter
+    def forecast_period(self, value):
+        self.__forecast_period = value
+
+    @property
+    def max_category(self):
+        if self.forecast_period != Outlooks.DAY4:
+            match = re.search(r"ENHANCED|SLIGHT|MODERATE|HIGH|MARGINAL|NO SEVERE]*", self.forecast_text, flags=0)
+            return match.group().capitalize()
+        else:
+            return 'Not available'
+
     def __init__(self, outlook):
-        self.forecast_period = outlook
-        self.url = self.base_url + self.__get__outlook_url()
-        self.base_url = None
-        self.forecast_text = self.__get_forecast_text()
-        self.max_category = self.__get_max_category()
+        self.__forecast_period = outlook
+        self.forecast_text = self.get_forecast_text()
         self.categorical_graphic = self.base_url + 'day1otlk_2000_prt.gif'
         self.probabilistic_tornado_graphic = self.base_url + 'day1probotlk_2000_torn_prt.gif'
         self.probabilistic_damaging_wind_graphic = None
         self.probabilistic_large_hail_graphic = None
 
-    @property
-    def base_url(self):
-        return r'https://www.spc.noaa.gov/products/outlook/'
-
-    @base_url.setter
-    def base_url(self, base_url):
-        self.base_url = base_url
-
-    def __get__outlook_url(self):
-        if self.forecast_period is Outlooks.DAY1:
-            return 'day1otlk.html'
-        elif self.forecast_period is Outlooks.DAY2:
-            return 'day2otlk.html'
-        elif self.forecast_period is Outlooks.DAY3:
-            return 'day3otlk.html'
-        elif self.forecast_period is Outlooks.DAY4:
-            self.base_url(r'https://www.spc.noaa.gov/products/exper/')
-            return 'day4-8/'
-        else:
-            raise IndexError("That outlook doesn't exist.")
-
-    def __get_forecast_text(self):
+    def get_forecast_text(self):
         r = requests.get(self.url)
         data = r.text
         soup = BeautifulSoup(data, "lxml")
         for text in soup.find('pre'):
             return text
 
-    def __get_max_category(self):
-        if self.forecast_period != Outlooks.DAY4:
-            match = re.search(r"ENHANCED|SLIGHT|MODERATE|HIGH|MARGINAL|NO SEVERE]*", self.forecast_text, flags=0)
-            return match.group()
-        else:
-            return 'Not available'
 
-
-o = ConvectiveOutlook(Outlooks.DAY4)
+o = ConvectiveOutlook(Outlooks.DAY1)
 print(o.forecast_text)
-print('The max category is: ' + o.max_category.capitalize())
+print('The max category is: ' + o.max_category)
